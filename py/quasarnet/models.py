c7c76f89249e0e1739339cee21cf493f2d9814b8
ngbusca <ngbusca@in2p3.fr>
2018-01-29 16:29:55 -0800

    fixed loss

--- py/quasarnet/models.py
+++ py/quasarnet/models.py
@@ -35,14 +35,15 @@ def QuasarNET(input_shape =  None, classes = 6, boxes = 13, nlines = 1, reg_conv
     X = BatchNormalization()(X)
     X = Activation('relu', name='fc_activation')(X)
 
-    X_class = Dense(classes, activation='softmax', name='fc_softmax' + str(classes), kernel_initializer = glorot_uniform())(X)
+    X_class = Dense(classes, activation='softmax', name='fc_softmax' + str(classes), kernel_initializer = glorot_uniform(), kernel_regularizer=regularizers.l2(reg_fc))(X)
     X_bal = Dense(1, activation='sigmoid', name='fc_bal', kernel_initializer = glorot_uniform())(X)
 
     outputs = [X_class, X_bal]
     X_box = []
     for i in range(nlines):
-        X_box_aux = Dense(boxes, activation='softmax', name='fc_box_{}'.format(i), kernel_initializer = glorot_uniform())(X)
-        X_offset_aux = Dense(boxes, activation='sigmoid', name='fc_offset_{}'.format(i), kernel_initializer = glorot_uniform(), kernel_regularizer=regularizers.l2(reg_fc))(X)
+        #X_box_aux = Dense(boxes, activation='softmax', name='fc_box_{}'.format(i), kernel_initializer = glorot_uniform())(X)
+        X_box_aux = Dense(boxes, activation='sigmoid', name='fc_box_{}'.format(i), kernel_initializer = glorot_uniform())(X)
+        X_offset_aux = Dense(boxes, activation='sigmoid', name='fc_offset_{}'.format(i), kernel_initializer = glorot_uniform())(X)
         X_box_aux = concatenate([X_box_aux, X_offset_aux], name="conc_box_{}".format(i))
         print "adding additional lines", X_box_aux.shape
         X_box.append(X_box_aux)
@@ -50,12 +51,14 @@ def QuasarNET(input_shape =  None, classes = 6, boxes = 13, nlines = 1, reg_conv
     for b in X_box:
         outputs.append(b)
 
+    nn_z = 10
     if len(X_box)==1:
-        Z = Dense(1, activation='relu', name="redshift", kernel_initializer = glorot_uniform())(X_box[0])
-        outputs.append(Z)
+        Z = Dense(nn_z, activation='relu', name="redshift_hidden", kernel_initializer = glorot_uniform())(X_box[0])
     elif len(X_box)>1:
-        Z = Dense(1, activation='relu', name="redshift", kernel_initializer = glorot_uniform())(concatenate(X_box))
-        outputs.append(Z)
+        Z = Dense(nn_z, activation='relu', name="redshift_hidden", kernel_initializer = glorot_uniform())(concatenate(X_box))
+    
+    Z = Dense(1, activation='relu', name='redshift', kernel_initializer = glorot_uniform())(Z)
+    outputs.append(Z)
 
     # Create model
     model = Model(inputs = X_input, outputs = outputs, name='QuasarNET')
@@ -65,11 +68,13 @@ def QuasarNET(input_shape =  None, classes = 6, boxes = 13, nlines = 1, reg_conv
 def custom_loss(y_true, y_pred):
     assert y_pred.shape[1]%2 == 0
     nboxes = y_pred.shape[1]/2
-    loss_class = K.categorical_crossentropy(y_true[...,0:nboxes], y_pred[...,0:nboxes])
+    #loss_class = K.categorical_crossentropy(y_true[...,0:nboxes], y_pred[...,0:nboxes])
+    loss_class = -K.mean(y_true[...,0:nboxes]*tf.log(K.clip(y_pred[...,0:nboxes], K.epsilon(), 1-K.epsilon())))
+    loss_class -= K.mean((1-y_true[...,0:nboxes])*tf.log(K.clip(1-y_pred[...,0:nboxes], K.epsilon(), 1-K.epsilon())))
     offset_true = y_true[...,nboxes:]
     offset_pred = y_pred[...,nboxes:]
     doffset = tf.subtract(offset_true, offset_pred)
-    doffset = tf.where(y_true[...,0:nboxes]==1, doffset, y_true[...,0:nboxes])
-    loss_offset = K.mean(tf.square(doffset))
+    doffset = tf.where(tf.equal(y_true[...,0:nboxes],1), doffset, tf.zeros_like(offset_true))
+    loss_offset = tf.multiply(tf.to_float(nboxes), K.mean(tf.square(doffset)))
 
     return tf.add(loss_class, loss_offset)
